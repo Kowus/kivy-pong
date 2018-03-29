e43de4dea7be0d718e33b5e08198380d1eb32668
Kowus <barnabas@elite-education.org>
2018-03-29 15:24:38 +0000

    PongBall class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty
 
 
 class PongGame(Widget):
@@ -9,6 +10,18 @@ class PongGame(Widget):
 class PongApp(App):
     def build(self):
         return PongGame()
+
+
+class PongBall(Widget):
+    # ball velocity
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    # ball motion
+    def move(self):
+        self.pos = Vector( * self.velocity) + self.pos
 
 
 if __name__ == '__main__':
